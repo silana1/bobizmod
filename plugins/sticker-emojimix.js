bd4b7c725b143521c3c9a97d9553a298a26032b0
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-08 19:19:57 +0100

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -3,7 +3,7 @@ import MessageType from '@adiwajshing/baileys'
 import fetch from 'node-fetch'
 import fs from "fs"
 let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝙾 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 #emojimix <emoji 1>&<emoji 2>*\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*#emojimix 🤨&😣*'
+if (!args[0]) throw 'هذا الامر يمكنك أن تقوم بمزج اثنين من ايموجي معين في ايموجي واحد مثال : \n *.emojimix* 😊&😄'
 let [emoji1, emoji2] = text.split`&`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
