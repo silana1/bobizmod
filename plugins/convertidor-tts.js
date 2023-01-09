ff872e87b28f59961cd027d35f112be23f111e13
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 18:36:48 +0100

    Update convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -1,42 +1,76 @@
-import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
-import { join } from 'path'
+import gtts from  node-gtts 
 
-const defaultLang = 'es'
+import { readFileSync, unlinkSync } from  fs 
+
+import { join } from  path 
+
+const defaultLang =  ar 
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 let lang = args[0]
-let text = args.slice(1).join(' ')
-if ((args[0] || '').length !== 2) {
+
+let text = args.slice(1).join(   )
+
+if ((args[0] ||   ).length !== 2) {
+
 lang = defaultLang
-text = args.join(' ')
+
+text = args.join(   )
+
 }
+
 if (!text && m.quoted?.text) text = m.quoted.text
 
 let res
+
 try { res = await tts(text, lang) }
+
 catch (e) {
-m.reply(e + '')
-text = args.join(' ')
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙰 𝙽𝙾𝚃𝙰 𝙳𝙴 𝚅𝙾𝚉, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} es Hola Mundo*`
+
+m.reply(e +   )
+
+text = args.join(   )
+
+if (!text) throw `تحويل نص لاوديو مثال \n *.tts بوبيز أحسن بوت في العالم*`
+
 res = await tts(text, defaultLang)
+
 } finally {
-if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+
+if (res) conn.sendFile(m.chat, res,  tts.opus , null, m, true)
+
 }}
-handler.help = ['tts <lang> <teks>']
-handler.tags = ['tools']
+
+handler.help = [ tts <lang> <teks> ]
+
+handler.tags = [ tools ]
+
 handler.command = /^g?tts$/i
+
 export default handler
 
-function tts(text, lang = 'es') {
+function tts(text, lang =  es ) {
+
 console.log(lang, text)
+
 return new Promise((resolve, reject) => {
+
 try {
+
 let tts = gtts(lang)
-let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
+
+let filePath = join(global.__dirname(import.meta.url),  ../tmp , (1 * new Date) +  .wav )
+
 tts.save(filePath, text, () => {
+
 resolve(readFileSync(filePath))
+
 unlinkSync(filePath)
+
 })
+
 } catch (e) { reject(e) }
+
 })}
+
