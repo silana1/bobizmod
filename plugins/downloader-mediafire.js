548f89c6fa853ad064e18469b02300ff98f8aa82
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 22:44:08 +0100

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,22 +1,42 @@
-import { mediafiredl } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/pbabuzyc7i8ord5/TheMystic-Bot-MD-master_%25285%2529.zip/file*`
-try {
-let res = await mediafiredl(args[0])
-let { url, url2, filename, ext, aploud, filesize, filesizeH } = await res
-let caption = `
-*📓 𝙽𝙾𝙼𝙱𝚁𝙴:* ${filename}
-*📁 𝙿𝙴𝚂𝙾:* ${filesizeH}
-*📄 𝚃𝙸𝙿𝙾:* ${ext}
+import { mediafiredl } from  @bochilteam/scraper 
 
-*⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* 
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+if (!args[0]) throw ` الامر خاص بتحميل ملفات الميديافاير مثال : \n *.mediafire* https://www.mediafire.com/download/i9qkk6onsxlyy7f`
+
+try {
+
+let res = await mediafiredl(args[0])
+
+let { url, url2, filename, ext, aploud, filesize, filesizeH } = await res
+
+let caption = `
+
+*📓 اسمه:* ${filename}
+
+*📁 حجمه:* ${filesizeH}
+
+*📄 نوعه:* ${ext}
+
+*⏳ المرجو الانتظار جاري تحميل الملف . . . .* 
+
 `.trim()
+
 m.reply(caption)
-await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+
+await conn.sendFile(m.chat, url, filename,   , m, null, { mimetype: ext, asDocument: true })
+
 } catch {
-await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/pbabuzyc7i8ord5/TheMystic-Bot-MD-master_%25285%2529.zip/file*')
+
+await m.reply( هذا الامر خاص بتحميل ملفات الميديافاير مثال : \n *.mediafire* https://www.mediafire.com/download/i9qkk6onsxlyy7f* )
+
 }}
-handler.help = ['mediafire'].map(v => v + ' <url>')
-handler.tags = ['downloader']
+
+handler.help = [ mediafire ].map(v => v +   <url> )
+
+handler.tags = [ downloader ]
+
 handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
+
 export default handler
+
