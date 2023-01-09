ba6697802825fc774160646e9211a0edd301d2e7
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 22:35:22 +0100

    Update downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -1,12 +1,24 @@
-import { googleImage } from '@bochilteam/scraper'
+import { googleImage } from  @bochilteam/scraper 
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} Minecraft*`
+
+if (!text) throw  هذا الامر خاص بتحميل الصور من غوغل مثال \n *.image* Free fire   
+
 const res = await googleImage(text)
+
 let image = await res.getRandom()
+
 let link = image
-let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
-conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)}
-handler.help = ['gimage <query>', 'imagen <query>']
-handler.tags = ['internet', 'tools']
+
+let captionn = `🔎 *♥:* ${text}\n🔗 *رابطها* ${link}\n🌎 *المصدر:* Google`
+
+conn.sendButton(m.chat, captionn, author, link, [[ 🔄 التالي... 🔄 , `#imagen ${text}`]], m)}
+
+handler.help = [ gimage <query> ,  imagen <query> ]
+
+handler.tags = [ internet ,  tools ]
+
 handler.command = /^(gimage|image|imagen)$/i
+
 export default handler
+
