77bd3f5cdab01e4387b1abcf2866e9b307aca450
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 09:39:49 +0100

    Update convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,12 +1,26 @@
 /* 𝐂𝐑𝐄𝐀𝐃𝐎 𝐏𝐎𝐑 https://github.com/BrunoSobrino */
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙶𝙸𝙵 𝙲𝙾𝙽 𝙰𝚄𝙳𝙸𝙾*`
+
+if (!m.quoted) throw `تحويل فيديو الى صورة متحركة GIF 
+
+مثال :  ترسل للبوت الفيديو الذي تريد تحويله لصورة متحركة ثم تشير اليه وتكتب  \n *.togifaud*`
+
 const q = m.quoted || m
-let mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) throw `*[❗] 𝙴𝙻 𝚃𝙸𝙿𝙾 𝙳𝙴 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 ${mime} 𝙽𝙾 𝙴𝚂 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾, 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙶𝙸𝙵 𝙲𝙾𝙽 𝙰𝚄𝙳𝙸𝙾*`
+
+let mime = (q.msg || q).mimetype ||   
+
+if (!/(mp4)/.test(mime)) throw `تحويل فيديو الى صورة متحركة GIF 
+
+مثال :  ترسل للبوت الفيديو الذي تريد تحويله لصورة متحركة ثم تشير اليه وتكتب  \n *.togifaud*`
+
 m.reply(global.wait)
+
 let media = await q.download()
-conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: '*ᴀϙᴜɪ ᴇsᴛᴀ sᴜ ɢɪғ ᴄᴏɴ ᴀᴜᴅɪᴏ, ᴀʟ ᴀʙʀɪʀʟᴏ sᴇ ʀᴇᴘʀᴏᴅᴜᴄᴇ ᴄᴏɴ ᴀᴜᴅɪᴏ*' }, { quoted: m })}
-handler.command = ['togifaud']
+
+conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption:  ʙᴏʙɪᴢ ʙᴏᴛ By Noureddine ouafy*  }, { quoted: m })}
+
+handler.command = [ togifaud ]
+
 export default handler
+
