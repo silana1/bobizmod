929edfb4d83ef3ea5857279d5ec63ef9a1733804
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 09:22:41 +0100

    Update _antiArab.js

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -8,7 +8,7 @@ let chat = global.db.data.chats[m.chat]
 
 if (isBotAdmin && chat.antiArab) {
 		
-if (m.sender.startsWith('212' || '212')) {
+if (m.sender.startsWith('62' || '62')) {
 global.db.data.users[m.sender].banned = true
 m.reply(`✳️ Anti árabes está activo para evitar spam\n\nHasta la próxima`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
