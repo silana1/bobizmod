b5e9a6db09266a22dbe35de18211939397655393
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 23:52:32 +0100

    Update downloader-playlist.js

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -1,22 +1,44 @@
-import { youtubeSearch } from '@bochilteam/scraper'
+import { youtubeSearch } from  @bochilteam/scraper 
+
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Begin you*`
+
+if (!text) throw `*هذا الامر لتحميل فيديوهات اليوتوب على شكل موسيقىو او فيديو او ملف يعني على شكل \n video / mp3 / audio/ document`
+
 try {
+
 const { video } = await youtubeSearch(text)
+
 const listSections = []
+
 let teks = [...video ].map(v => {
+
 switch (v.type) {
-case 'video': {
+
+case  video : {
+
 listSections.push([`${v.title}`, [
-['Video 🎥', `${usedPrefix}ytmp4 ${v.url}`, `descargar: ${v.title} (${v.url})`],
-['Videodoc 🎥', `${usedPrefix}ytmp4doc ${v.url}`, `descargar: ${v.title} (${v.url})`],    
-['Audio 🎧', `${usedPrefix}ytmp3 ${v.url}`, `descargar: ${v.title} (${v.url})`],
-['Audiodoc 🎧', `${usedPrefix}ytmp3doc ${v.url}`, `descargar: ${v.title} (${v.url})`]
+
+[ Video 🎥 , `${usedPrefix}ytmp4 ${v.url}`, `تحميل: ${v.title} (${v.url})`],
+
+[ Videodoc 🎥 , `${usedPrefix}ytmp4doc ${v.url}`, `تحميل: ${v.title} (${v.url})`],    
+
+[ Audio 🎧 , `${usedPrefix}ytmp3 ${v.url}`, `تحميل: ${v.title} (${v.url})`],
+
+[ Audiodoc 🎧 , `${usedPrefix}ytmp3doc ${v.url}`, `تحميل: ${v.title} (${v.url})`]
+
 ]])
-}}}).filter(v => v).join('\n\n========================\n\n')
-conn.sendList(m.chat, ' 『 𝗠𝗨𝗦𝗜𝗖𝗔 𝗥𝗘𝗟𝗔𝗖𝗜𝗢𝗡𝗔𝗗𝗔 』', `𝐌𝐮𝐬𝐢𝐜𝐚 𝐫𝐞𝐥𝐚𝐜𝐢𝐨𝐧𝐚𝐝𝐚 𝐜𝐨𝐧: ${args.join(" ")}`, '𝐄𝐥𝐢𝐣𝐚 𝐮𝐧𝐚 𝐨𝐩𝐜𝐢𝐨𝐧 𝐲 𝐩𝐫𝐞𝐜𝐢𝐨𝐧𝐞 𝐄𝐧𝐯𝐢𝐚𝐫', '[♦ 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 ♦]', listSections, m)
+
+}}}).filter(v => v).join( \n\n========================\n\n )
+
+conn.sendList(m.chat,   『 https://instagram.com/noureddine_ouafy 』 , `البحث عن : ${args.join(" ")}`,  ♥ ,  [♦ نتائج البحث اختر ما تريده  ♦] , listSections, m)
+
 } catch {
-await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*')
+
+await m.reply( *[❗error❗] هناك مشكلة في البوت المرجو الاعادة لاحقا او التواصل مع صاحب البوت \n instagram.com/noureddine_ouafy* )
+
 }}
+
 handler.command = /^playlist|playlist2$/i
+
 export default handler
+
