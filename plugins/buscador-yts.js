85f5290e67cc40e6e39362a1a84f6c4d1d30dfb9
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-09 09:31:24 +0100

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,25 +1,25 @@
-import { youtubeSearch } from '@bochilteam/scraper'
+import { youtubeSearch } from  @bochilteam/scraper 
 let handler = async (m, { text }) => {
-if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
+if (!text) throw  هذا الامر خاص بالبحث في اليوتوب مثال : \n *.yts* noureddine ouafy 
 const { video, channel } = await youtubeSearch(text)
 let teks = [...video, ...channel].map(v => {
 switch (v.type) {
-case 'video': return `
+case  video : return `
 📌 *${v.title}* (${v.url})
-⌚ 𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗: ${v.durationH}
-⏲️ 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘 ${v.publishedTime}
+⌚ 𝙳: ${v.durationH}
+⏲️ 𝙿𝚞𝚋𝚕𝚒𝚌 ${v.publishedTime}
 👁️ ${v.view} 𝚟𝚒𝚜𝚝𝚊𝚜
 `.trim()
-case 'channel': return `
+case  channel : return `
 📌 *${v.channelName}* (${v.url})
 🧑‍🤝‍🧑 _${v.subscriberH} 𝚜𝚞𝚜𝚌𝚛𝚒𝚙𝚝𝚘𝚛𝚎𝚜_
 🎥 ${v.videoCount} 𝚟𝚒𝚍𝚎𝚘𝚜
 `.trim()
 }
-}).filter(v => v).join('\n\n========================\n\n')
+}).filter(v => v).join( \n\n========================\n\n )
 m.reply(teks)
 }
-handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['tools']
+handler.help = [  ,  earch ].map(v =>  yts  + v +   <pencarian> )
+handler.tags = [ tools ]
 handler.command = /^yts(earch)?$/i
 export default handler
