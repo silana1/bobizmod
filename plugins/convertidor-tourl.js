fe986ba0f22159b9ab2f384353c76ea19074f98b
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-10 14:20:34 +0100

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,30 +1,16 @@
 import uploadFile from'../lib/uploadFile.js'
-
 import uploadImage from'../lib/uploadImage.js'
-
 let handler = async (m) => {
-
 let q = m.quoted ? m.quoted : m
-
 let mime = (q.msg || q).mimetype ||   
-
-if (!mime) throw  رفع الصور الى موقع telegraph uploader \n والحصول على رابط الصورة \n  يكفي فقط ان ترسل للبوت صورة او فيديو ثم تشير اليه وتكتب \n *.tourl* 
-
+if (!mime) throw'رفع الصور الى موقع telegraph uploader \n والحصول على رابط الصورة \n  يكفي فقط ان ترسل للبوت صورة او فيديو ثم تشير اليه وتكتب \n *.tourl*' 
 let media = await q.download()
-
 let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-
 let link = await (isTele ? uploadImage : uploadFile)(media)
-
 m.reply(`*😌:* ${link}`)
-
 }
-
 handler.help = [ tourl <reply image> ]
-
 handler.tags = [ sticker ]
-
 handler.command = /^(upload|tourl)$/i
-
 export default handler
 
