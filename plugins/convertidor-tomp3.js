00773e5e2b10086c0cddaf0c1fc4b8bf2d2c6e4c
silana1 <107745987+silana1@users.noreply.github.com>
2023-01-10 18:44:31 +0100

    Update convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -1,28 +1,15 @@
-import { toAudio } from  ../lib/converter.js 
-
+import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-
 let q = m.quoted ? m.quoted : m
-
 let mime = (q || q.msg).mimetype || q.mediaType ||   
-
 if (!/video|audio/.test(mime)) throw `*قم بالاشارة للفيديو الذي تريد ان تحوله لمقطع موسيقى واكتب نفس الامر الذي كتبته للتو*`
-
 let media = await q.download()
-
-if (!media) throw  *هناك خطب ما المرجو مراسلة صاحب البوت * 
-
+if (!media) throw'*هناك خطب ما المرجو مراسلة صاحب البوت*'
 let audio = await toAudio(media,  mp4 )
-
-if (!audio.data) throw  *أعد المحاولة مجددا * 
-
+if (!audio.data) throw  '*أعد المحاولة مجددا*'
 conn.sendMessage(m.chat, { audio: audio.data,  mimetype:  audio/mpeg  }, { quoted: m })
-
 }
-
-handler.alias = [ tomp3 ,  toaudio ]
-
+handler.alias = [ 'tomp3 ,  toaudio']
 handler.command = /^to(mp3|audio)$/i
-
 export default handler
 
